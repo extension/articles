6e4a82bbbb10170b756e6f15e121a50af2f6e9b4
Aaron Hundley <aaron.hundley@extension.org>
2009-06-11 22:06:46 +0000

    Lighten the load of the expert controller and send widget-related code that resided there to join the rest of the widget code in the widgets controller.

--- app/controllers/ask/widgets_controller.rb
+++ app/controllers/ask/widgets_controller.rb
@@ -62,7 +62,7 @@ class Ask::WidgetsController < ApplicationController
     user = User.find(12)
     
     @widget.set_fingerprint(user)
-    @widget_url = url_for(:controller => 'ask/expert', :action => :widget, :location => location_str, :county => county_str, :id => @widget.fingerprint, :only_path => false)  
+    @widget_url = url_for(:controller => 'ask/widgets', :action => :widget, :location => location_str, :county => county_str, :id => @widget.fingerprint, :only_path => false)  
     @widget.widget_url = @widget_url
     @widget.author = user.login
     
@@ -96,6 +96,116 @@ class Ask::WidgetsController < ApplicationController
       end
     else
       do_404
+    end
+  end
+  
+  # ask widget pulled from remote iframe
+  def widget
+    if params[:location]
+      @location = Location.find_by_abbreviation(params[:location].strip)
+      if params[:county] and @location
+        @county = County.find_by_name_and_location_id(params[:county].strip, @location.id)
+      end 
+    end
+    
+    @fingerprint = params[:id]
+    @host_name = request.host_with_port
+    render :layout => false
+  end
+
+  def widget_assignees
+    @widget = Widget.find(params[:id])
+    if !@widget
+      flash[:notice] = "The widget you specified does not exist."
+      redirect_to :controller => :account, :action => :widget_preferences
+      return
+    end
+    @widget_assignees = @widget.assignees
+  end
+  
+  # TODO: make akismet work
+  def create_from_widget
+    if request.post?
+      
+      begin
+        # setup the question to be saved and fill in attributes with parameters
+        create_question
+        if !@submitted_question.valid?
+          @argument_errors = @submitted_question.errors.full_messages.join('<br />')  
+          raise ArgumentError
+        end
+        
+        # validate akismet key and log an error if something goes wrong with connecting to 
+        # akismet web service to verify the key
+        #begin
+        #  valid_key = has_verified_akismet_key?
+        #rescue Exception => ex
+        #  logger.error "Error verifying akismet key #{AppConfig.configtable['akismet_key']} at #{time.now.to_s}.\nError: #{ex.message}"
+        #end
+        
+        # check the akismet web service to see if the submitted question is spam or not
+        #if valid_key
+        #  begin
+        #    @submitted_question.spam = is_spam? @submitted_question.to_akismet_hash
+        #  rescue Exception => exc
+        #    logger.error "Error checking submitted question for spam via Akismet at #{Time.now.to_s}. Akismet webservice might be experiencing problems.\nError: #{exc.message}"
+        #  end
+        #else
+        #  logger.error "Akismet key #{AppConfig.configtable['akismet_key']} was not validated at #{Time.now.to_s}."
+        #end
+
+        if @submitted_question.save
+          if @submitted_question.assignee
+            AskMailer.deliver_assigned(@submitted_question, url_for(:controller => 'ask/expert', :action => 'question', :id => @submitted_question.id), request.host)
+          end
+          render :layout => false
+        else
+          raise InternalError
+        end
+      
+      rescue ArgumentError => ae
+        @status = '400 (argument error)'
+        render :template => 'ask/widgets/status', :status => 400, :layout => false
+        return
+      #rescue Exception => e
+      #  @status = '500 (internal error)'
+      #  render :template => 'ask/expert/status', :status => 500, :layout => false
+      #  return
+      end
+    end
+  end
+  
+  private
+  
+  def create_question
+    # remove all whitespace in question before putting into db.
+    params[:submitted_question].collect{|key, val| params[:submitted_question][key] = val.strip}
+    widget = Widget.find_by_fingerprint(params[:id].strip) if params[:id]
+    
+    @submitted_question = SubmittedQuestion.new(params[:submitted_question])
+    @submitted_question.widget = widget if widget
+    @submitted_question.widget_name = widget.name if widget
+    @submitted_question.user_ip = request.remote_ip
+    @submitted_question.user_agent = request.env['HTTP_USER_AGENT']
+    @submitted_question.referrer = request.env['HTTP_REFERER']
+    @submitted_question.status = SubmittedQuestion::SUBMITTED_TEXT
+    @submitted_question.status_state = SubmittedQuestion::STATUS_SUBMITTED
+    @submitted_question.external_app_id = 'widget'
+    
+    # check to see if question has location associated with it
+    incoming_location = params[:location].strip if params[:location] and params[:location].strip != '' and params[:location].strip != Location::ALL
+    
+    if incoming_location
+      location = Location.find_by_fipsid(incoming_location.to_i)
+      @submitted_question.location = location if location
+    end
+    
+    # check to see if question has county and said location associated with it
+    incoming_county = params[:county].strip if params[:county] and params[:county].strip != '' and params[:county].strip != County::ALL
+    
+    if incoming_county and location
+      county = County.find_by_fipsid_and_location_id(incoming_county.to_i, location.id)
+      @submitted_question.county = county if county
     end
   end
   
