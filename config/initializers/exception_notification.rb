b464a488cb1a37ae1b26c2c7524d82376db52fbf
Jason Adam Young <jasonadamyoung@gmail.com>
2009-06-25 21:05:55 +0000

    use darmok for exception_notification email

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,4 +1,4 @@
 # exception notifier settings
 ExceptionNotifier.exception_recipients = AppConfig.configtable['mail_errors_to']
-ExceptionNotifier.sender_address = %("identity-#{AppConfig.configtable['mail_label']}" <exdev@extension.org>)
-ExceptionNotifier.email_prefix = "[identity-#{AppConfig.configtable['mail_label']}] "
+ExceptionNotifier.sender_address = %("darmok-#{AppConfig.configtable['mail_label']}" <exdev@extension.org>)
+ExceptionNotifier.email_prefix = "[darmok-#{AppConfig.configtable['mail_label']}] "
