faf11218d19414825bd6ac3aa78e84a78aa2fd1d
Jason Adam Young <jasonadamyoung@gmail.com>
2009-06-30 00:41:42 +0000

    typo in named scope

--- app/models/search_question.rb
+++ app/models/search_question.rb
@@ -27,7 +27,7 @@ class SearchQuestion < ActiveRecord::Base
     else
       match_string = "#{query_string}"
     end
-    {:select => "activity_objects.*, MATCH(content,fulltitle) AGAINST ('#{match_string}') as match_score", :conditions => ["MATCH(content,fulltitle) AGAINST ('#{match_string}')"]}
+    {:select => "#{self.table_name}.*, MATCH(content,fulltitle) AGAINST ('#{match_string}') as match_score", :conditions => ["MATCH(content,fulltitle) AGAINST ('#{match_string}')"]}
   }
 
 
