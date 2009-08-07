74a3622fd4d3bebb8c5ed7630a43e865dc1508f9
Aaron Hundley <aaron.hundley@extension.org>
2009-08-07 17:37:00 +0000

    Replace peoplebot references with 'System'

--- app/helpers/aae/question_helper.rb
+++ app/helpers/aae/question_helper.rb
@@ -8,35 +8,36 @@
 module Aae::QuestionHelper
   
 def stringify_submitted_question_event(sq_event)
+  
+  if sq_event.initiated_by_id == User.systemuserid
+    initiated_by_full_name = "System"
+  else
+    initiated_by_full_name = sq_event.initiated_by.fullname
+  end
+  
   case sq_event.event_state
   when SubmittedQuestionEvent::ASSIGNED_TO 
-    if sq_event.initiated_by_id == User.systemuserid
-      initiated_by_full_name = "System"
-    else
-      initiated_by_full_name = sq_event.initiated_by.fullname
-    end
-
     reassign_msg = "Assigned to <strong><a href='/aae/profile/#{sq_event.subject_user.login}'>#{sq_event.subject_user.fullname}</a></strong> by <strong>#{initiated_by_full_name}</strong> <span> #{humane_date(sq_event.created_at)}</span>"
     reassign_msg = reassign_msg + " <span>Comments: #{sq_event.response}</span>" if sq_event.response
     return reassign_msg 
   when SubmittedQuestionEvent::RESOLVED 
-    return "Resolved by <strong>#{sq_event.initiated_by.fullname}</strong> <span> #{humane_date(sq_event.created_at)}</span>"
+    return "Resolved by <strong>#{initiated_by_full_name}</strong> <span> #{humane_date(sq_event.created_at)}</span>"
   when SubmittedQuestionEvent::NO_ANSWER
-    return "No answer available was sent from <strong>#{sq_event.initiated_by.fullname}</strong><span> #{humane_date(sq_event.created_at)}</span>"
+    return "No answer available was sent from <strong>#{initiated_by_full_name}</strong><span> #{humane_date(sq_event.created_at)}</span>"
   when SubmittedQuestionEvent::MARKED_SPAM
-    return "Marked as spam by <strong>#{sq_event.initiated_by.fullname}</strong> <span> #{humane_date(sq_event.created_at)}</span>"
+    return "Marked as spam by <strong>#{initiated_by_full_name}</strong> <span> #{humane_date(sq_event.created_at)}</span>"
   when SubmittedQuestionEvent::MARKED_NON_SPAM
-    return "Marked as non-spam by <strong>#{sq_event.initiated_by.fullname}</strong> <span> #{humane_date(sq_event.created_at)}</span>"
+    return "Marked as non-spam by <strong>#{initiated_by_full_name}</strong> <span> #{humane_date(sq_event.created_at)}</span>"
   when SubmittedQuestionEvent::REJECTED
-    return "Question Rejected by <strong>#{sq_event.initiated_by.fullname}</strong> <span> #{humane_date(sq_event.created_at)}</span>"
+    return "Question Rejected by <strong>#{initiated_by_full_name}</strong> <span> #{humane_date(sq_event.created_at)}</span>"
   when SubmittedQuestionEvent::REACTIVATE
-    return "Question Reactivated by <strong>#{sq_event.initiated_by.fullname}</strong> <span> #{humane_date(sq_event.created_at)}</span>"
+    return "Question Reactivated by <strong>#{initiated_by_full_name}</strong> <span> #{humane_date(sq_event.created_at)}</span>"
   when SubmittedQuestionEvent::RECATEGORIZED
-    return "Question Recategorized by <strong>#{sq_event.initiated_by.fullname}</strong> <span> #{humane_date(sq_event.created_at)}</span>"
+    return "Question Recategorized by <strong>#{initiated_by_full_name}</strong> <span> #{humane_date(sq_event.created_at)}</span>"
   when SubmittedQuestionEvent::WORKING_ON
-    return "Question worked on by <strong>#{sq_event.initiated_by.fullname}</strong> <span> #{humane_date(sq_event.created_at)}</span>"
+    return "Question worked on by <strong>#{initiated_by_full_name}</strong> <span> #{humane_date(sq_event.created_at)}</span>"
   else
-    return "Submitted question #{sq_event.submitted_question.id.to_s} #{SubmittedQuestion.convert_state_to_text(sq_event.event_state)} #{((sq_event.subject_user) ? sq_event.subject_user.fullname : '')} by #{sq_event.initiated_by.fullname} <span> #{humane_date(sq_event.created_at)}</span>"
+    return "Submitted question #{sq_event.submitted_question.id.to_s} #{SubmittedQuestion.convert_state_to_text(sq_event.event_state)} #{((sq_event.subject_user) ? sq_event.subject_user.fullname : '')} by #{initiated_by_full_name} <span> #{humane_date(sq_event.created_at)}</span>"
   end
 end
 
