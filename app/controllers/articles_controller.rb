36dbb9c6d629692db29238641bd6f6affc0260c9
Ben MacNeill <ben.macneill@gmail.com>
2009-08-28 18:18:12 +0000

    learning lessons hook for GA

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -119,7 +119,7 @@ class ArticlesController < ApplicationController
       set_titletag("#{@article.title} - eXtension News")
     elsif @article_bucket_names.include?('learning lessons')
       set_title('Learning', "Don't just read. Learn.")
-      set_titletag("#{@article.title} - eXtension")
+      set_titletag("#{@article.title} - eXtension Learning Lessons")
     else
       set_title("#{@article.title} - eXtension", "Articles from our resource area experts.")
       set_titletag("#{@article.title} - eXtension")
