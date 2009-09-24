fb7469e745c112ab08249539f68063a6cb3ac026
Suzanne Dirkers <smdirker@ncsu.edu>
2009-09-24 20:01:25 +0000

    speed up dashboard by not doing db calls for each submitted question but only their submitted_question_events

--- app/controllers/aae/dashboard_controller.rb
+++ app/controllers/aae/dashboard_controller.rb
@@ -28,10 +28,8 @@ class Aae::DashboardController < ApplicationController
     today = (Time.now).strftime("%Y-%m-%d %H:%M:%S")
     
     filteroptions = {:category => @category, :location => @location, :county => @county, :source => @source}
-    @submitted_questions = SubmittedQuestion.submitted.filtered(filteroptions).ordered(@order).listdisplayincludes    #.paginate(:page => params[:page])
- #   @submitted_questions = SubmittedQuestion.find(:all, :conditions => "submitted_questions.status_state = #{SubmittedQuestion::STATUS_SUBMITTED} AND submitted_questions.spam = FALSE", :order => "created_at DESC")
-  #  @assignees = User.find(:all, :select => "submitted_questions.created_at sqcreated_at, submitted_questions.id sqid, users.id uid, last_login_at, user_id, last_name, first_name, login", :joins => "join submitted_questions on submitted_questions.user_id=users.id", :conditions =>  "submitted_questions.status_state = #{SubmittedQuestion::STATUS_SUBMITTED} AND submitted_questions.spam = FALSE", :order => "submitted_questions.created_at DESC")
-   # @assignees_events = SubmittedQuestionEvent.find(:all, :conditions => " submitted_question_id IN (#{@submitted_questions.join(',')})", :order => "submitted_question_id")
+  #  @submitted_questions = SubmittedQuestion.submitted.filtered(filteroptions).ordered(@order).listdisplayincludes    #.paginate(:page => params[:page]
+    @assignees = SubmittedQuestion.get_assignees(last6months, today, SubmittedQuestion.filterconditions(filteroptions)[:conditions], SubmittedQuestion.filterconditions(filteroptions)[:include])
     @assgnscompls= User.get_num_times_assigned(last6months,today , " and resolved_by=subject_user_id ", SubmittedQuestion.filterconditions(filteroptions)[:conditions],SubmittedQuestion.filterconditions(filteroptions)[:include])
     @totalassgns = User.get_num_times_assigned(last6months,today, "", SubmittedQuestion.filterconditions(filteroptions)[:conditions], SubmittedQuestion.filterconditions(filteroptions)[:include])
     @avgscompl=User.get_avg_resp_time_only(last6months, today, SubmittedQuestion.filterconditions(filteroptions)[:conditions], SubmittedQuestion.filterconditions(filteroptions)[:include])
