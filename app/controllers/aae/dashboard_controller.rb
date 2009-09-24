41285cfcca221b364d98f3897d3b51ea64b3715f
Suzanne Dirkers <smdirker@ncsu.edu>
2009-09-24 14:45:30 +0000

    add in most recent login

--- app/controllers/aae/dashboard_controller.rb
+++ app/controllers/aae/dashboard_controller.rb
@@ -19,8 +19,8 @@ class Aae::DashboardController < ApplicationController
     end
     
     #set the instance variables based on parameters
-    list_view
-    set_filters
+  #  list_view
+  #  set_filters
   #  filter_string_helper
     
     t = Time.now
@@ -29,14 +29,14 @@ class Aae::DashboardController < ApplicationController
     
     filteroptions = {:category => @category, :location => @location, :county => @county, :source => @source}
     @submitted_questions = SubmittedQuestion.submitted.filtered(filteroptions).ordered(@order).listdisplayincludes    #.paginate(:page => params[:page])
+ #   @submitted_questions = SubmittedQuestion.find(:all, :conditions => "submitted_questions.status_state = #{SubmittedQuestion::STATUS_SUBMITTED} AND submitted_questions.spam = FALSE", :order => "created_at DESC")
+  #  @assignees = User.find(:all, :select => "submitted_questions.created_at sqcreated_at, submitted_questions.id sqid, users.id uid, last_login_at, user_id, last_name, first_name, login", :joins => "join submitted_questions on submitted_questions.user_id=users.id", :conditions =>  "submitted_questions.status_state = #{SubmittedQuestion::STATUS_SUBMITTED} AND submitted_questions.spam = FALSE", :order => "submitted_questions.created_at DESC")
     @assgnscompls= User.get_num_times_assigned(last6months,today , " and resolved_by=subject_user_id ", SubmittedQuestion.filterconditions(filteroptions)[:conditions],SubmittedQuestion.filterconditions(filteroptions)[:include])
     @totalassgns = User.get_num_times_assigned(last6months,today, "", SubmittedQuestion.filterconditions(filteroptions)[:conditions], SubmittedQuestion.filterconditions(filteroptions)[:include])
     @avgscompl=User.get_avg_resp_time_only(last6months, today, SubmittedQuestion.filterconditions(filteroptions)[:conditions], SubmittedQuestion.filterconditions(filteroptions)[:include])
     
-    #get set of last activities
- #   acts = Activity.displayactivity.find(:all, :order => 'created_at DESC', :group => "user_id")
-   @last_activity={}
-  #  @last_activity = acts.map { |act| @last_activity[act.user_id] = act }
+    
+  
   end
   
   
