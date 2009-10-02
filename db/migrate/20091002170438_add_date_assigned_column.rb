6e8a82558acc34b4e20243c8f529c4884eb02ceb
Jason Adam Young <jasonadamyoung@gmail.com>
2009-10-02 18:59:07 +0000

    change column name.   - folks if you already ran this migration, please reimport a copy of prod_darmok and run your migrations again.

--- db/migrate/20091002170438_add_date_assigned_column.rb
+++ db/migrate/20091002170438_add_date_assigned_column.rb
@@ -1,11 +1,11 @@
 class AddDateAssignedColumn < ActiveRecord::Migration
   def self.up
      # add column to sq for date-assigned
-     add_column :submitted_questions, :last_assign_date, :datetime
+     add_column :submitted_questions, :last_assigned_at, :datetime
 
    end
 
    def self.down
-     remove_column :submitted_questions, :last_assign_date
+     remove_column :submitted_questions, :last_assigned_at
    end
 end
