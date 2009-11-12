a21f608781f4deb3f32554792e8b9645a3dd1b6b
Jason Adam Young <jasonadamyoung@gmail.com>
2009-11-12 15:50:59 +0000

    migration changes, you know the drill :-)

--- db/migrate/20091102140333_convert_institutions_to_communities.rb
+++ db/migrate/20091102140333_convert_institutions_to_communities.rb
@@ -5,7 +5,6 @@ class ConvertInstitutionsToCommunities < ActiveRecord::Migration
     remove_column :communities, :listeligible
     # create needed institution columns and indexes inside the communities table
     add_column :communities, "location_id", :integer, :default => 0
-    add_column :communities, "normalizedname", :string    
     add_column :communities, "public_uri", :string    
     add_column :communities, "shared_logo", :boolean, :default => false
     add_column :communities, "referer_domain", :string
@@ -28,16 +27,21 @@ class ConvertInstitutionsToCommunities < ActiveRecord::Migration
     execute "UPDATE users, old_institutions SET users.organization_name = old_institutions.name, users.organization_entrytype = old_institutions.entrytype WHERE users.institution_id = old_institutions.id"
     
     # create official institution communities from old_institutions table from the landgrant and federal institutions
-    execute "INSERT INTO communities (entrytype,memberfilter,name,created_at,updated_at,created_by,uri,normalizedname,location_id,public_uri,referer_domain,shared_logo,show_in_public_list,institution_code) SELECT 3,1,name,created_at,updated_at,1,uri,normalizedname,location_id,public_uri,referer_domain,shared_logo,show_in_public_list,code FROM old_institutions WHERE old_institutions.entrytype IN (1,3)"
+    execute "INSERT INTO communities (entrytype,memberfilter,name,created_at,updated_at,created_by,uri,location_id,public_uri,referer_domain,shared_logo,show_in_public_list,institution_code) SELECT 3,1,name,created_at,updated_at,1,uri,location_id,public_uri,referer_domain,shared_logo,show_in_public_list,code FROM old_institutions WHERE old_institutions.entrytype IN (1,3)"
     
     # set primary institution id based on a name match to the newly imported communities
     execute "UPDATE users,communities SET users.temp_institution_id = communities.id WHERE users.organization_name = communities.name AND communities.entrytype = 3"        
   
     # finally, create community connection records for these
     execute "INSERT INTO communityconnections (user_id,community_id,connectiontype,connectioncode,connected_by,created_at,updated_at) SELECT id,temp_institution_id,'member',#{Communityconnection::PRIMARY},1,created_at,NOW() from users WHERE retired = 0 and vouched = 1 and temp_institution_id !=0 and temp_institution_id IS NOT NULL"  
-  
+
+    # clear out the organization names for those we just added, to make it easier to create additional migrations later - we don't need to keep that data
+    execute "UPDATE users SET users.organization_name = '', users.organization_entrytype = 0 WHERE temp_institution_id !=0 and temp_institution_id IS NOT NULL "        
+        
     # drop temporary institution id
     remove_column :users, "temp_institution_id"
+    
+    
   end
 
   def self.down
--- db/migrate/20091112133938_normalize_texas_institutions.rb
+++ db/migrate/20091112133938_normalize_texas_institutions.rb
@@ -1,33 +1,56 @@
 class NormalizeTexasInstitutions < ActiveRecord::Migration
   def self.up
+    # insert statement
+    insert_statement = "INSERT IGNORE INTO communityconnections (user_id,community_id,connectiontype,connectioncode,connected_by,created_at,updated_at)"
+    
     # create an official institution for Texas AgriLife Research
     research = Community.create(:entrytype => Community::INSTITUTION, :created_by => User.systemuserid, :location_id => 53, :name => 'Texas AgriLife Research')
-    
     # add users to the new research community
-    corequerystring = "INSERT IGNORE INTO communityconnections (user_id,community_id,connectiontype,connectioncode,connected_by,created_at,updated_at) SELECT id,#{research.id},'member',#{Communityconnection::PRIMARY},1,created_at,NOW() from users WHERE retired = 0 and vouched = 1 and organization_name ="
-    execute "#{corequerystring} 'Texas AgriLife Research'"
-    execute "#{corequerystring} 'Texas A&M University'"
-    execute "#{corequerystring} 'Texas A&M'"
-    execute "#{corequerystring} 'Texas Agricultural Experiment Station'"
-
+    research_select = "SELECT id,#{research.id},'member',#{Communityconnection::PRIMARY},1,created_at,NOW() from users WHERE retired = 0 and vouched = 1 and organization_name ="
+    research_orgs = []
+    research_orgs << 'Texas AgriLife Research'
+    research_orgs << 'Texas A&M University'
+    research_orgs << 'Texas A&M'
+    research_orgs << 'Texas Agricultural Experiment Station'
+    
+    research_orgs.each do |org|
+      execute "#{insert_statement} #{research_select} '#{org}'"
+    end
+    
     # get Texas AgriLife Extension community - change the name
     agrilife = Community.find_by_name('Texas AgriLife Extension')
     agrilife.update_attribute(:name,'Texas AgriLife Extension Service')
     
     # add users to the new research community
-    corequerystring = "INSERT IGNORE INTO communityconnections (user_id,community_id,connectiontype,connectioncode,connected_by,created_at,updated_at) SELECT id,#{agrilife.id},'member',#{Communityconnection::PRIMARY},1,created_at,NOW() from users WHERE retired = 0 and vouched = 1 and organization_name ="
-    execute "#{corequerystring} 'Texas Agrilife Extension Service'"
-    execute "#{corequerystring} 'Texas A&M University - Texas AgriLife Extension'"
-    execute "#{corequerystring} 'Texas AgriLife'"
-    execute "#{corequerystring} 'Texas AgriLife Extension/Research'"
-    execute "#{corequerystring} 'AgriLife Extension, Texas A&M University'"
-    execute "#{corequerystring} 'Texas AgriLife Extension Service District'"
-    execute "#{corequerystring} 'Texas AgriLife Extension Service/Agricultural & Environmental Safety'"
-    execute "#{corequerystring} 'Texas AgriLife Extension Services'"
-    execute "#{corequerystring} 'Texas AgriLife Res & Ext Center'"
-    execute "#{corequerystring} 'Texas AgriLife Research & Extension Center, Dallas'"
-    execute "#{corequerystring} 'Texas A&M University System'"
-    execute "#{corequerystring} 'Texas Cooperative Extension'"
+    agrilife_select = "SELECT id,#{agrilife.id},'member',#{Communityconnection::PRIMARY},1,created_at,NOW() from users WHERE retired = 0 and vouched = 1 and organization_name ="
+    agrilife_orgs = []
+    agrilife_orgs << 'Texas Agrilife Extension Service'
+    agrilife_orgs << 'Texas A&M University - Texas AgriLife Extension'
+    agrilife_orgs << 'Texas AgriLife'
+    agrilife_orgs << 'Texas AgriLife Extension/Research'
+    agrilife_orgs << 'AgriLife Extension, Texas A&M University'
+    agrilife_orgs << 'Texas AgriLife Extension Service District'
+    agrilife_orgs << 'Texas AgriLife Extension Service/Agricultural & Environmental Safety'
+    agrilife_orgs << 'Texas AgriLife Extension Services'
+    agrilife_orgs << 'Texas AgriLife Res & Ext Center'
+    agrilife_orgs << 'Texas AgriLife Research & Extension Center, Dallas'
+    agrilife_orgs << 'Texas A&M University System'
+    agrilife_orgs << 'Texas Cooperative Extension'
+    
+    agrilife_orgs.each do |org|
+      execute "#{insert_statement} #{agrilife_select} '#{org}'"
+    end
+
+    # okay, now for the people we just put into Institution buckets, get rid of their organization name and type
+    # yes I know I could do a join and just one query, but this loop won't take long and it's easier to read
+    research_orgs.each do |org|
+      execute "UPDATE users SET users.organization_name = '', users.organization_entrytype = 0 WHERE users.organization_name = '#{org}'"        
+    end
+    
+    agrilife_orgs.each do |org|
+      execute "UPDATE users SET users.organization_name = '', users.organization_entrytype = 0 WHERE users.organization_name = '#{org}'"        
+    end
+    
   end
 
   def self.down
