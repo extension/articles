135ffe057b93a3f171ae420f5129b2286ffa020c
Jason Adam Young <jasonadamyoung@gmail.com>
2009-11-18 19:23:48 +0000

    whoops.  missing lib.

---
+++ lib/classconstant_utilities.rb
@@ -0,0 +1,23 @@
+# === COPYRIGHT:
+#  Copyright (c) 2005-2009 North Carolina State University
+#  Developed with funding for the National eXtension Initiative.
+# === LICENSE:
+#  BSD(-compatible)
+#  see LICENSE file or view at http://about.extension.org/wiki/LICENSE
+
+module ClassconstantUtilities
+  
+  def code_to_constant_string(code)
+    constantslist = self.constants
+    constantslist.each do |c|
+      value = self.const_get(c)
+      if(value.is_a?(Fixnum) and code == value)
+        return c
+      end
+    end
+  
+    # if we got here?  return nil
+    return nil
+  end
+  
+end
