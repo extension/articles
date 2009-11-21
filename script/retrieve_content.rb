c3801bd94c0415aba3d302d1435df8b0e002169f
Jason Adam Young <jasonadamyoung@gmail.com>
2009-11-21 15:04:01 +0000

    remove retrieve_deletes for Articles from script.  DO NOT RELEASE TO PRODUCTION.

--- script/retrieve_content.rb
+++ script/retrieve_content.rb
@@ -71,10 +71,11 @@ def retrieve_content_for_datatype(objectklass,options)
   begin
     result = objectklass.retrieve_content(options)
     puts "Result: #{result.inspect}"
-    if(objectklass == Article)
-      result = objectklass.retrieve_deletes(options)
-      puts "Deletes Result: #{result.inspect}"
-    end
+    # TODO: Warning!  don't release this to production! - jayoung 2009/11/21
+    # if(objectklass == Article)
+    #   result = objectklass.retrieve_deletes(options)
+    #   puts "Deletes Result: #{result.inspect}"
+    # end
   rescue ContentRetrievalError => exception
     puts exception.message
   end
