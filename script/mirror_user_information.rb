000de29c74f7ddbc0d10ad33201ba2c7858da2ad
Aaron Hundley <aaron.hundley@extension.org>
2010-01-07 20:55:23 +0000

    Fixed script that's cron'ed to populate the user mirror db for Identity that faq updates it's users from.

--- script/mirror_user_information.rb
+++ script/mirror_user_information.rb
@@ -38,22 +38,20 @@ def update_from_identity_users(connection,userinfodb,mydatabase)
   identitysql += "users.first_name as first_name,users.last_name as last_name,"
   identitysql += "users.title as title,users.retired as retired,"
   identitysql += "users.created_at as created_at, users.updated_at as updated_at,"
-  identitysql += "locations.abbreviation as state,counties.name as county,institutions.name as university"
+  identitysql += "locations.abbreviation as state,counties.name as county"
   identitysql += " FROM users"
   identitysql += " LEFT JOIN locations ON users.location_id = locations.id"
   identitysql += " LEFT JOIN counties ON users.county_id = counties.id"
-  identitysql += " LEFT JOIN institutions ON users.institution_id = institutions.id"
-#  identitysql += " LEFT JOIN communities ON (users.institution_id = communities.id AND communities.type = 'Institution')"
   identitysql += " WHERE (users.vouched = 1 OR users.retired = 1)"
   identitysql += " AND users.emailconfirmed = 1"
   # keep peoplebot out of the data
   identitysql += " AND users.id != 1"
   
   
-  sql = "REPLACE INTO #{userinfodb}.users (id,login,email,first_name,last_name,title,retired,created_at,updated_at,state,county,university)"
+  sql = "REPLACE INTO #{userinfodb}.users (id,login,email,first_name,last_name,title,retired,created_at,updated_at,state,county)"
   sql +=  " SELECT identitysourcedata.userid, identitysourcedata.login,identitysourcedata.email,identitysourcedata.first_name,identitysourcedata.last_name,"
   sql +=  "identitysourcedata.title,identitysourcedata.retired,identitysourcedata.created_at,identitysourcedata.updated_at,"
-  sql +=  "identitysourcedata.state,identitysourcedata.county,identitysourcedata.university"
+  sql +=  "identitysourcedata.state,identitysourcedata.county"
   sql +=  " FROM (#{identitysql}) as identitysourcedata"
   
   # execute the sql
