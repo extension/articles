8e31bb1e1a5de30eb787ddc25e4cac91f05e825b
Jason Adam Young <jasonadamyoung@gmail.com>
2010-02-12 20:18:41 +0000

    Resolve issue #1002 - provide empty question list checks.

--- app/controllers/aae/dashboard_controller.rb
+++ app/controllers/aae/dashboard_controller.rb
@@ -17,19 +17,21 @@ class Aae::DashboardController < ApplicationController
       list_view_error(err_msg)
       return
     end
-    
+
     #set the instance variables based on parameters 
-   list_view
-   set_filters
-   filter_string_helper
-  
-   @filteroptions = {:category => @category, :location => @location, :county => @county, :source => @source}
-   @submitted_questions = SubmittedQuestion.submitted.filtered(@filteroptions).ordered(@order).listdisplayincludes.all
-   # get the assignees
-   assignee_ids = @submitted_questions.collect(&:assignee).map(&:id)
-   # handling rates and averages
-   @handling_counts = User.aae_handling_event_count({:group_by_id => true, :limit_to_handler_ids => assignee_ids,:submitted_question_filter => @filteroptions.merge({:notrejected => true})})
-   @handling_averages = User.aae_handling_average({:group_by_id => true, :limit_to_handler_ids => assignee_ids,:submitted_question_filter => @filteroptions.merge({:notrejected => true})})  
+    list_view
+    set_filters
+    filter_string_helper
+
+    @filteroptions = {:category => @category, :location => @location, :county => @county, :source => @source}
+    @submitted_questions = SubmittedQuestion.submitted.filtered(@filteroptions).ordered(@order).listdisplayincludes.all
+    if(!@submitted_questions.blank?)
+      # get the assignees
+      assignee_ids = @submitted_questions.collect(&:assignee).map(&:id)
+      # handling rates and averages
+      @handling_counts = User.aae_handling_event_count({:group_by_id => true, :limit_to_handler_ids => assignee_ids,:submitted_question_filter => @filteroptions.merge({:notrejected => true})})
+      @handling_averages = User.aae_handling_average({:group_by_id => true, :limit_to_handler_ids => assignee_ids,:submitted_question_filter => @filteroptions.merge({:notrejected => true})})  
+    end
   end
   
   
