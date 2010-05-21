134135da88bf6141eee77bfc915b8a5aea33ea12
athundle <aaron.hundley@extension.org>
2010-05-21 10:31:46 -0400

    Added in more error checking and form validation of AaE questions along with changing the question confirmation page to allow editing of the question on the previous page instead of the current page to make sure there is not a disconnect between search and asking a question and for data submission reasons. Other changes were also sprinkled throughout.

--- app/controllers/ask_controller.rb
+++ app/controllers/ask_controller.rb
@@ -9,7 +9,7 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class AskController < ApplicationController  
-  has_rakismet :only => [:submit_question]
+  has_rakismet :only => [:submission_form]
   before_filter :login_optional
   
   layout 'pubsite'
@@ -18,91 +18,131 @@ class AskController < ApplicationController
   
   def index
     @right_column = false
-    session[:return_to] = params[:redirect_to]
-    flash.now[:googleanalytics] = '/ask-an-expert-form'
-    
     set_title("Ask an Expert - eXtension", "New Question")
-    set_titletag("Ask an Expert - eXtension")
-
+    
     # if we are editing
-    if params[:submitted_question]
+    if params[:q]
       flash.now[:googleanalytics] = '/ask-an-expert-edit-question'
       set_titletag("Edit your Question - eXtension")
-      begin
-        if(params[:public_user])
-          @public_user = PublicUser.find_and_update_or_create_by_email(params[:public_user])
+      @question = params[:q].sanitize
+    else
+      flash.now[:googleanalytics] = '/ask-an-expert-form'
+      set_titletag("Ask an Expert - eXtension")
+    end    
+  end
+  
+  def submission_form
+    @categories = [""].concat(Category.launched_content_categories.map{|c| [c.name, c.id]})
+    @location_options = get_location_options
+    @county_options = get_county_options
+    
+    if request.get?  
+      if !params[:q].blank?  
+        @submitted_question = SubmittedQuestion.new(:asked_question => params[:q].sanitize)
+        @right_column = false
+        session[:return_to] = params[:redirect_to]
+        flash.now[:googleanalytics] = '/ask-an-expert-submission-form'
+    
+        set_title("Ask an Expert - eXtension", "New Question")
+        set_titletag("Ask an Expert - eXtension")
+
+        if(!session[:public_user_id].nil?)
+          @public_user = PublicUser.find_by_id(session[:public_user_id]) || PublicUser.new
         elsif(!@currentuser.nil?)
           # let's get cute and fill in the name/email
           @public_user = PublicUser.new(:email => @currentuser.email, :first_name => @currentuser.first_name, :last_name => @currentuser.last_name)
         else
           @public_user = PublicUser.new
-        end
-        
-        @submitted_question = SubmittedQuestion.new(params[:submitted_question])
-        @submitted_question.location_id = params[:location_id]
-        @submitted_question.county_id = params[:county_id]
-        @submitted_question.setup_categories(params[:aae_category], params[:subcategory])
-        @top_level_category = @submitted_question.top_level_category if @submitted_question.top_level_category
-        @sub_category = @submitted_question.sub_category.id if @submitted_question.sub_category
-        
-        if @top_level_category 
-          @sub_category_options = [""].concat(@top_level_category.children.map{|sq| [sq.name, sq.id]})
-        else
-          @sub_category_options = [""]
-        end
-        # run validator to display any input errors
-        @submitted_question.valid?
-        @public_user.valid?
-      rescue
-        @public_user = PublicUser.new
-        @submitted_question = SubmittedQuestion.new
+        end  
+      # question was blank
+      else
+        flash[:notice] = "Please fill in the question field before submitting."
+        redirect_to :action => :index
       end
+    # Question asker submits the question (ie. POST request)
     else
-      if(!session[:public_user_id].nil?)
-        @public_user = PublicUser.find_by_id(session[:public_user_id]) || PublicUser.new
-      elsif(!@currentuser.nil?)
-        # let's get cute and fill in the name/email
-        @public_user = PublicUser.new(:email => @currentuser.email, :first_name => @currentuser.first_name, :last_name => @currentuser.last_name)
-      else
-        @public_user = PublicUser.new
+      # need to use filter params here
+      if(params[:submitted_question][:asked_question].blank? or params[:public_user].blank?)
+        return_error = "Please fill in all required fields."
+      elsif params[:public_user][:email].blank? or params[:public_email_confirmation].blank?
+        return_error = "Please enter an email address and email address confirmation."
+      elsif params[:public_user][:email].strip != params[:public_email_confirmation].strip
+        return_error = "Your email address confirmation does not match.<br />Please make sure your email address and confirmation match up."
       end
-      @submitted_question = SubmittedQuestion.new
-    end
-    
-    @location_options = get_location_options
-    @county_options = get_county_options
-    
-    @categories = [""].concat(Category.launched_content_categories.map{|c| [c.name, c.id]})
-  end
-  
-  def submission_form
-    if request.post? and !params[:q].blank?
-      @submitted_question = SubmittedQuestion.new(:asked_question => params[:q])
-      @right_column = false
-      session[:return_to] = params[:redirect_to]
-      flash.now[:googleanalytics] = '/ask-an-expert-submission-form'
-    
-      set_title("Ask an Expert - eXtension", "New Question")
-      set_titletag("Ask an Expert - eXtension")
 
+      @public_user = PublicUser.find_and_update_or_create_by_email(params[:public_user])
 
-      if(!session[:public_user_id].nil?)
-        @public_user = PublicUser.find_by_id(session[:public_user_id]) || PublicUser.new
-      elsif(!@currentuser.nil?)
-        # let's get cute and fill in the name/email
-        @public_user = PublicUser.new(:email => @currentuser.email, :first_name => @currentuser.first_name, :last_name => @currentuser.last_name)
-      else
-        @public_user = PublicUser.new
+      @submitted_question = SubmittedQuestion.new(params[:submitted_question])
+      @submitted_question.location_id = params[:location_id]
+      @submitted_question.county_id = params[:county_id]
+      @submitted_question.setup_categories(params[:aae_category], params[:subcategory])
+      @submitted_question.status = 'submitted'
+      @submitted_question.user_ip = request.remote_ip
+      @submitted_question.user_agent = request.env['HTTP_USER_AGENT']
+      @submitted_question.referrer = (request.env['HTTP_REFERER']) ? request.env['HTTP_REFERER'] : ''
+      @submitted_question.status_state = SubmittedQuestion::STATUS_SUBMITTED
+      @submitted_question.status = SubmittedQuestion::SUBMITTED_TEXT
+      @submitted_question.external_app_id = 'www.extension.org'
+      @submitted_question.public_user = @public_user
+
+      # error check for submitted question and public user records 
+      
+      if !@public_user
+        # create a new instance variable for public_user since it wasn't saved so the form can be repopulated
+        @public_user = PublicUser.new(params[:public_user]) 
+        # we know it wasn't saved, but let's see why
+        if !@public_user.valid? and return_error.blank?
+          return_error = "Errors occured when saving:<br />" + @public_user.errors.full_messages.join('<br />')
+        end
+      end
+       
+      if !@submitted_question.valid? and return_error.blank?
+        return_error = "Errors occured when saving:<br />" + @submitted_question.errors.full_messages.join('<br />')
       end
       
-      @location_options = get_location_options
-      @county_options = get_county_options
-    
-      @categories = [""].concat(Category.launched_content_categories.map{|c| [c.name, c.id]})
-    else
-      flash[:notice] = "Please fill in the question field before submitting." if params[:q].blank?
-      redirect_to :action => :index
+      # end of error check for submitted question and public user records
+      
+      if !return_error.blank?
+        if top_level_category = @submitted_question.top_level_category
+          @sub_category_options = [""].concat(top_level_category.children.map{|sq| [sq.name, sq.id]})
+          if sub_category = @submitted_question.sub_category
+            @sub_category = sub_category.id
+          end
+        end
+        flash.now[:notice] = return_error
+        render nil
+        return
+      end
+
+      # for easier akismet checking, set the submitter_email attribute from the associated public_user
+      @submitted_question.submitter_email = @public_user.email
+
+      # check for spam
+      begin
+        @submitted_question.spam = @submitted_question.spam?
+      rescue Exception => ex
+        logger.error "Error checking submitted question from pubsite aae form for spam via Akismet at #{Time.now.to_s}. Akismet webservice might be experiencing problems.\nError: #{ex.message}"
+      end
+      
+      if !params[:file_attachment].blank?
+        @file_attachment = FileAttachment.create(params[:file_attachment]) 
+        @submitted_question.file_attachments << @file_attachment
+      end
+
+      @submitted_question.save
+
+      session[:public_user_id] = @public_user.id
+
+      flash[:notice] = 'Your question has been submitted and the answer will be sent to your email. Our experts try to answer within 48 hours.'
+      flash[:googleanalytics] = '/ask-an-expert-question-submitted'
+
+      if session[:return_to]
+        redirect_to(session[:return_to]) 
+      else
+        redirect_to home_url
+      end
     end
+    # end of question submission POST request
   end
   
   def question_confirmation
@@ -110,6 +150,8 @@ class AskController < ApplicationController
       if params[:q].blank? 
         flash[:notice] = "Please fill in the question field before submitting."
         redirect_to :action => :index
+      else
+        @question = params[:q].sanitize
       end
     else
       redirect_to :action => :index
@@ -217,67 +259,6 @@ class AskController < ApplicationController
     
     flash.now[:warning] = "The email address you entered does not match the email used to submit the question. Please check the email address and try again."
     render :template => 'ask/question_signin'
-  end
-  
-  def submit_question
-    if request.post?
-      @public_user = PublicUser.find_and_update_or_create_by_email(params[:public_user])
-      
-      @submitted_question = SubmittedQuestion.new(params[:submitted_question])
-      @submitted_question.location_id = params[:location_id]
-      @submitted_question.county_id = params[:county_id]
-      @submitted_question.setup_categories(params[:aae_category], params[:subcategory])
-      @submitted_question.status = 'submitted'
-      @submitted_question.user_ip = request.remote_ip
-      @submitted_question.user_agent = request.env['HTTP_USER_AGENT']
-      @submitted_question.referrer = (request.env['HTTP_REFERER']) ? request.env['HTTP_REFERER'] : ''
-      @submitted_question.status_state = SubmittedQuestion::STATUS_SUBMITTED
-      @submitted_question.status = SubmittedQuestion::SUBMITTED_TEXT
-      @submitted_question.external_app_id = 'www.extension.org'
-      @submitted_question.public_user = @public_user
-      
-      if !params[:file_attachment].blank?
-        @file_attachment = FileAttachment.create(params[:file_attachment]) 
-        @submitted_question.file_attachments << @file_attachment
-      end
-      
-      # for easier akismet checking, set the submitter_email attribute from the associated public_user
-      if(!@public_user.nil?)
-        @submitted_question.submitter_email = @public_user.email
-      else
-        # TODO: this really should display a validation error
-        flash[:notice] = 'There was an error saving your question. Please try again.'
-        redirect_to :action => 'index'
-        return
-      end
-        
-    
-      # let's check for spam
-      begin
-        @submitted_question.spam = @submitted_question.spam?
-      rescue Exception => ex
-        logger.error "Error checking submitted question from pubsite aae form for spam via Akismet at #{Time.now.to_s}. Akismet webservice might be experiencing problems.\nError: #{ex.message}"
-      end
-    
-      if !@submitted_question.valid? || !@public_user.valid? || !@submitted_question.save
-        flash[:notice] = 'There was an error saving your question. Please try again.'
-        redirect_to :action => 'index'
-        return
-      end
-      
-      session[:public_user_id] = @public_user.id
-    
-      flash[:notice] = 'Your question has been submitted and the answer will be sent to your email. Our experts try to answer within 48 hours.'
-      flash[:googleanalytics] = '/ask-an-expert-question-submitted'
-      if session[:return_to]
-        redirect_to(session[:return_to]) 
-      else
-        redirect_to '/'
-      end
-    else
-      flash[:warning] = "Please enter your question via the ask an expert form"
-      redirect_to ask_form_url
-    end
   end
   
   def edit_question
