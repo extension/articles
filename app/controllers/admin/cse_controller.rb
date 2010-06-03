abece82a3488800617b5e9fc3fed87b2e2716ce9
jerobins <james@robinsonhouse.com>
2010-06-03 13:57:02 -0400

    place holders

--- app/controllers/admin/cse_controller.rb
+++ app/controllers/admin/cse_controller.rb
@@ -5,40 +5,27 @@
 #  BSD(-compatible)
 #  see LICENSE file or view at http://about.extension.org/wiki/LICENSE
 
+require 'gdata'
+require 'lib/gdata-cse'
+
 class Admin::CseController < ApplicationController
   before_filter :admin_required
   before_filter :check_purgatory
   before_filter :turn_off_right_column
-  
+
   layout 'pubsite'
 
   def index
     set_titletag('Manage CSE Links - Pubsite Admin')
-    @logos = Logo.sponsorlogos.find(:all, :conditions => { :parent_id => nil }, :order => 'created_at DESC')
   end
 
   def new
-    @logo = Logo.new
   end
 
   def create
-    @logo = Logo.new(params[:logo])
-    @logo.logotype = Logo::SPONSOR
-
-    if @logo.save
-      flash[:notice] = 'Logo was successfully uploaded.'
-      AdminEvent.log_event(@currentuser, AdminEvent::CREATE_LOGO,{:logo_id => @logo.id, :logo_filename => @logo.filename})      
-      redirect_to(admin_logos_url)
-    else
-      render(:action => "new")
-    end
   end
 
   def destroy
-    @logo = Logo.find(params[:id])
-    AdminEvent.log_event(@currentuser, AdminEvent::DELETE_LOGO,{:logo_id => @logo.id, :logo_filename => @logo.filename})
-    @logo.destroy
-    redirect_to(admin_logos_url)
-  end    
-    
+  end
+
 end
