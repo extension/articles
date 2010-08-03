91c0e59db93711e4fdb2132a3e0c953fee498e9c
Jason Adam Young <jasonadamyoung@gmail.com>
2010-08-03 18:28:22 -0400

    add time_zone column to users model, override convenience setter/getter routines to ensure that the timezone string is stored in mysql in a way that we can use in custom mysql queries where needed.

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ActiveRecord::Base
   serialize :additionaldata
   ordered_by :default => "last_name,first_name ASC"
   
+  DEFAULT_TIMEZONE = 'America/New_York'
+  
   
   STATUS_CONTRIBUTOR = 0
   STATUS_REVIEW = 1
@@ -224,6 +226,44 @@ class User < ActiveRecord::Base
      {:conditions => " users.location_id = #{location.id}", :group => "users.id"}
     end
   }
+  
+  # override timezone writer/reader
+  # returns Eastern by default, use convert=false
+  # when you need a timezone string that mysql can handle
+  def time_zone(convert=true)
+    tzinfo_time_zone_string = read_attribute(:time_zone)
+    if(tzinfo_time_zone_string.blank?)
+      tzinfo_time_zone_string = DEFAULT_TIMEZONE
+    end
+      
+    if(convert)
+      reverse_mappings = ActiveSupport::TimeZone::MAPPING.invert
+      if(reverse_mappings[tzinfo_time_zone_string])
+        reverse_mappings[tzinfo_time_zone_string]
+      else
+        nil
+      end
+    else
+      tzinfo_time_zone_string
+    end
+  end
+  
+  def time_zone=(time_zone_string)
+    mappings = ActiveSupport::TimeZone::MAPPING
+    if(mappings[time_zone_string])
+      write_attribute(:time_zone, mappings[time_zone_string])
+    else
+      write_attribute(:time_zone, nil)
+    end
+  end
+  
+  # since we return a default string from timezone, this routine
+  # will allow us to check for a null/empty value so we can
+  # prompt people to come set one.
+  def has_time_zone?
+    tzinfo_time_zone_string = read_attribute(:timezone)
+    return (!tzinfo_time_zone_string.blank?)
+  end
   
   # override login write
   def login=(loginstring)
