ecf14d89361a1921c938fa31e32b4426d89a4e39
Jason Adam Young <jasonadamyoung@gmail.com>
2010-08-04 17:06:03 -0400

    not sure if there's index corruption with this or not, but remove and add to make sure there's not.

--- db/migrate/20100804200528_change_learn_connectiontype.rb
+++ db/migrate/20100804200528_change_learn_connectiontype.rb
@@ -1,6 +1,8 @@
 class ChangeLearnConnectiontype < ActiveRecord::Migration
   def self.up
+    remove_index(:learn_connections, ['learn_session_id','connectiontype'])
     change_column :learn_connections, :connectiontype, :integer
+    add_index(:learn_connections, ['learn_session_id','connectiontype'])
   end
 
   def self.down
