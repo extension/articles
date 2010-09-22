9769dd53249b66bd0e2e0428fceedcc0ca4c3c91
Jason Adam Young <jasonadamyoung@gmail.com>
2010-09-22 14:53:37 -0400

    schema change

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100917170322) do
+ActiveRecord::Schema.define(:version => 20100922182828) do
 
   create_table "activities", :force => true do |t|
     t.datetime "created_at"
