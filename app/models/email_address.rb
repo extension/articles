251e6756dc24afec376000402184a5f6201cd491
Jason Adam Young <jasonadamyoung@gmail.com>
2010-10-20 10:20:25 -0400

    convenience routines for the EmailAddress class for domain matching and more.

--- app/models/email_address.rb
+++ app/models/email_address.rb
@@ -6,6 +6,36 @@
 #  see LICENSE file or view at http://about.extension.org/wiki/LICENSE
 class EmailAddress
   
+  attr_accessor :email_address, :parsed_email_address, :is_valid_address
+  
+  def initialize(email_address)
+    @email_address = email_address
+    begin
+      @parsed_email_address = TMail::Address.parse(email_address)
+    rescue TMail::SyntaxError
+      return nil
+    else
+      return self
+    end
+  end
+    
+  def base_domain
+    @parsed_email_address.domain.split('.').slice(-2, 2).join(".") rescue nil
+  end
+    
+  def domain
+    @parsed_email_address.domain
+  end
+    
+  def local
+    @parsed_email_address.local
+  end
+  
+  def inspect
+    @email_address
+  end
+  
+  # maintaining because it's used elsewhere
   def self.is_valid_address?(email_address)
     begin
       TMail::Address.parse(email_address)
