1d957639639106b1c15018b4544071f92892f959
Jason Adam Young <jasonadamyoung@gmail.com>
2010-10-21 15:09:23 -0400

    no reply address handling

--- app/models/app_config.rb
+++ app/models/app_config.rb
@@ -56,6 +56,7 @@ class AppConfig
     @@configtable['emailsettings']['aae_public'] = {'address' => 'ask-an-expert@extension.org', 'name' => 'eXtension Ask an Expert Response', 'bcc' => 'aaepublic.bcc.mirror@extension.org'}
     @@configtable['emailsettings']['aae_internal'] = {'address' => 'aae-notify@extension.org', 'name' => 'Do Not Reply - eXtension Ask an Expert Notification', 'bcc' => 'aaenotify.bcc.mirror@extension.org'}
     @@configtable['emailsettings']['aae_escalation'] = {'address' => 'aae-escalation@extension.org', 'name' => 'Do Not Reply - eXtension Ask an Expert Notification', 'bcc' => 'aaenotify.bcc.mirror@extension.org'}
+    @@configtable['emailsettings']['aae_noreply'] = {'address' => 'aae-noreply@extension.org', 'name' => 'Do Not Reply - eXtension Ask an Expert Notification', 'bcc' => 'aaenotify.bcc.mirror@extension.org'}
     @@configtable['emailsettings']['people'] = {'address' => 'peoplemail@extension.org', 'name' => 'eXtension People Notification - Do Not Reply', 'bcc' => 'people.bcc.mirror@extension.org', 'review' => 'eXtensionHelp@extension.org'}
     @@configtable['emailsettings']['default'] = {'address' => 'noreply@extension.org', 'name' => 'eXtension Notification', 'bcc' => 'default.bcc.mirror@extension.org'}
     @@configtable['emailsettings']['deploy'] = {'address' => 'exdev@extension.org', 'name' => 'eXDevBot', 'bcc' => 'deploys.mirror@extension.org'}
