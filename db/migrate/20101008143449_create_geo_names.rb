f49b34b2b0e560a51ff6a58d033bddfbf24267d0
Jason Adam Young <jasonadamyoung@gmail.com>
2010-11-08 13:41:48 -0500

    geo_names db from populated places data: http://geonames.usgs.gov/domestic/download_data.htm

---
+++ db/migrate/20101008143449_create_geo_names.rb
@@ -0,0 +1,32 @@
+class CreateGeoNames < ActiveRecord::Migration
+  def self.up
+    create_table "geo_names", :force => true do |t|
+      t.string  "feature_name",       :limit => 121
+      t.string  "feature_class",      :limit => 51
+      t.string  "state_abbreviation", :limit => 3
+      t.string  "state_code",         :limit => 3
+      t.string  "county",             :limit => 101
+      t.string  "county_code",        :limit => 4
+      t.string  "lat_dms",            :limit => 8
+      t.string  "long_dms",           :limit => 9
+      t.float   "lat"                
+      t.float   "long"               
+      t.string  "source_lat_dms",     :limit => 8
+      t.string  "source_long_dms",    :limit => 9
+      t.float   "source_lat"         
+      t.float   "source_long"        
+      t.integer "elevation"
+      t.string  "map_name"
+      t.string  "create_date_txt"
+      t.string  "edit_date_txt"
+      t.date    "create_date"
+      t.date    "edit_date"
+    end
+    
+    add_index("geo_names",["feature_name","state_abbreviation","county"], :name => 'name_state_county_ndx')
+  end
+
+  def self.down
+    drop_table "geo_names"
+  end
+end
