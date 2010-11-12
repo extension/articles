afddcd84fdd64b1769c60684d6552de81926161d
Jason Adam Young <jasonadamyoung@gmail.com>
2010-11-12 15:48:13 -0500

    convert widget urls to a belongs_to :location and belongs_to :county

---
+++ db/migrate/20101110142234_more_widget_fields.rb
@@ -0,0 +1,51 @@
+class MoreWidgetFields < ActiveRecord::Migration
+  def self.up
+    add_column(:widgets,:show_location,:boolean)
+    add_column(:widgets,:enable_tags,:boolean)
+    add_column(:widgets,:community_id,:integer)
+    add_column(:widgets,:location_id,:integer)
+    add_column(:widgets,:county_id,:integer)
+    add_column(:widgets,:old_widget_url,:string)
+    
+    execute "UPDATE widgets SET old_widget_url = widgeturl"
+    
+    Widget.reset_column_information
+    
+    # set location and county for each widget, and write every widget to be
+    # http://www.extension.org/widget/tracking/#{trackingcode}
+    Widget.all.each do |widget|
+      if(widget.widgeturl =~ %r!https*://\w+\.extension\.org/widget/tracking/(\w+)/(\w+)!)
+        # probably has a location
+        trackingcode = $1
+        location_abbreviation = $2
+        if(location = Location.find_by_abbreviation(location_abbreviation))
+          widget.location = location
+          if(widget.widgeturl =~ %r!https*://\w+\.extension\.org/widget/tracking/(\w+)/(\w+)/(.+)!)
+            # probably has a county
+            # unescape due to URL encoded values like Prince%20George's
+            county_name = URI.unescape($3)
+            # the quote value is here because of quoted values like Prince%20George's
+            if(county = County.find(:first, :conditions => "name = #{ActiveRecord::Base.quote_value(county_name)} and location_id = #{location.id}"))
+              widget.county = county
+            end
+          end
+        end
+        widget.widgeturl = "http://www.extension.org/widget/tracking/#{trackingcode}"
+        widget.save
+      elsif(widget.widgeturl =~ %r!https*://\w+\.extension\.org/widget/tracking/(\w+)!)
+        trackingcode = $1
+        widget.widgeturl = "http://www.extension.org/widget/tracking/#{trackingcode}"
+        widget.save
+      end 
+    end
+    
+    # special case for widget #708 - which has ?location=AL in it
+    execute("UPDATE widgets SET location_id = 11 where id = 708")
+    
+
+  end
+
+  def self.down
+    # no going back.
+  end
+end
