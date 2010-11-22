5906396f4ec13c684807264b78ace8bb8bbf003e
Jason Adam Young <jasonadamyoung@gmail.com>
2010-11-22 10:42:25 -0500

    add ip info to expert responses as well.

--- app/models/submitted_question.rb
+++ app/models/submitted_question.rb
@@ -132,12 +132,28 @@ def add_resolution(sq_status, resolver, response, signature = nil, contributing_
   case sq_status
     when STATUS_RESOLVED    
       self.update_attributes(:status => SubmittedQuestion.convert_to_string(sq_status), :status_state =>  sq_status, :resolved_by => resolver, :current_response => response, :resolver_email => resolver.email, :current_contributing_question => contributing_question, :resolved_at => t.strftime("%Y-%m-%dT%H:%M:%SZ"))  
-      @response = Response.new(:resolver => resolver, :submitted_question => self, :response => response, :sent => true, :contributing_question_id => contributing_question, :signature => signature)
+      @response = Response.new(:resolver => resolver, 
+                               :submitted_question => self, 
+                               :response => response,
+                               :user_ip => request.remote_ip,
+                               :user_agent => (request.env['HTTP_USER_AGENT']) ? request.env['HTTP_USER_AGENT'] : '',
+                               :referrer => (request.env['HTTP_REFERER']) ? request.env['HTTP_REFERER'] : '',  
+                               :sent => true, 
+                               :contributing_question_id => contributing_question, 
+                               :signature => signature)
       @response.save
       SubmittedQuestionEvent.log_resolution(self)    
     when STATUS_NO_ANSWER
       self.update_attributes(:status => SubmittedQuestion.convert_to_string(sq_status), :status_state =>  sq_status, :resolved_by => resolver, :current_response => response, :resolver_email => resolver.email, :current_contributing_question => contributing_question, :resolved_at => t.strftime("%Y-%m-%dT%H:%M:%SZ"))  
-      @response = Response.new(:resolver => resolver, :submitted_question => self, :response => response, :sent => true, :contributing_question_id => contributing_question, :signature => signature)
+      @response = Response.new(:resolver => resolver, 
+                               :submitted_question => self, 
+                               :response => response, 
+                               :user_ip => request.remote_ip,
+                               :user_agent => (request.env['HTTP_USER_AGENT']) ? request.env['HTTP_USER_AGENT'] : '',
+                               :referrer => (request.env['HTTP_REFERER']) ? request.env['HTTP_REFERER'] : '', 
+                               :sent => true, 
+                               :contributing_question_id => contributing_question, 
+                               :signature => signature)
       @response.save
       SubmittedQuestionEvent.log_no_answer(self)  
     when STATUS_REJECTED
