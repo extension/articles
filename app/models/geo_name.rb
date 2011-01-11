d9a441bf0fb83142e447e6666d8cbff5ff05f2de
Jason Adam Young <jasonadamyoung@gmail.com>
2011-01-11 12:16:09 -0500

    add find_by_geoip logic to GeoName, extend the query so that it finds Ben.

--- app/models/geo_name.rb
+++ app/models/geo_name.rb
@@ -6,5 +6,18 @@
 #  see LICENSE file or view at http://about.extension.org/wiki/LICENSE
 
 class GeoName < ActiveRecord::Base
+  
+  
+  def self.find_by_geoip(ipaddress = AppConfig.configtable['request_ip_address'])
+    if(geoip_data = Location.get_geoip_data(ipaddress))
+      if(geoip_data[:country_code] == 'US')
+        self.find(:first, :conditions => "feature_name = '#{geoip_data[:city]}' and map_name = '#{geoip_data[:city]}' and state_abbreviation = '#{geoip_data[:region]}'")
+      else
+        return nil
+      end
+    else
+      return nil
+    end
+  end
 
 end
