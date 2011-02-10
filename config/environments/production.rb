f82660aed4b9c89f392e925cc2ca4a7b4a59f1df
Jason Adam Young <jasonadamyoung@gmail.com>
2011-02-10 12:36:04 -0500

    more newrelic.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,8 @@
 # Settings specified here will take precedence over those in config/environment.rb
 
+# production only gems
+config.gem 'newrelic_rpm'
+
 # The production environment is meant for finished, "live" apps.
 # Code is not reloaded between requests
 config.cache_classes = true
