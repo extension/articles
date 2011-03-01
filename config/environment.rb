f91f71f767f1fbbd18f05f300e02ff606a6a1218
Jason Adam Young <jasonadamyoung@gmail.com>
2011-02-28 23:30:18 -0500

    require fake_arel

--- config/environment.rb
+++ config/environment.rb
@@ -34,6 +34,7 @@ Rails::Initializer.run do |config|
   config.gem 'geoip', :source => 'http://systems.extension.org/rubygems/'
   # required for email parsing
   config.gem 'mail'
+  config.gem 'fake_arel'
   
   # TODO: still need pubsite gems - http://justcode.extension.org/issues/show/521
 
