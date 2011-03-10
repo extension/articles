0dd187aaffde40af480b4221d53b9cac2a4202e6
Jason Adam Young <jasonadamyoung@gmail.com>
2011-03-10 17:03:30 -0500

    reprocess script changes

--- script/reprocess_all_content_links.rb
+++ script/reprocess_all_content_links.rb
@@ -1,38 +1,94 @@
 #!/usr/bin/env ruby
 require 'rubygems'
-require 'trollop'
+require 'thor'
 require 'net/http'
 require 'uri'
 
-commandline_options = Trollop::options do
-  opt(:environment,"Rails environment to start", :short => 'e', :default => 'production')
+
+class LinkManager < Thor
+  include Thor::Actions
+  
+  # these are not the tasks that you seek
+  no_tasks do
+    # load rails based on environment
+    
+    def load_rails(environment)
+      if !ENV["RAILS_ENV"] || ENV["RAILS_ENV"] == ""
+        ENV["RAILS_ENV"] = environment
+      end
+      require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
+    end
+    
+    def recreate_primary_links(verbose = false)
+      # dump the links table
+      Link.connection.execute('truncate table links;')      
+      page_count = 1
+      puts "Creating content links for each page"
+      Page.all.each do |page|
+        if(verbose)
+          puts "Processing Page: #{page.id} ##{page_count}"
+        end
+        page.create_primary_link
+        page_count += 1
+      end
+      return page_count
+    end
+    
+    def recreate_linkings(verbose = false)
+      # dump the linkings table
+      Linking.connection.execute('truncate table linkings;')      
+      page_count = 1
+      overall_links = {}
+      puts "Processing in-page links"
+      Page.all.each do |page|
+        if(verbose)
+          puts "Processing Page: #{page.id} (#{page.datatype}) ##{page_count}"
+        end
+        links = page.convert_links
+        page.save
+        if(verbose)
+          puts "Links: #{links.inspect}"
+        end
+        links.keys.each do |key|
+          if(overall_links[key])
+            overall_links[key] += links[key]
+          else
+            overall_links[key] = links[key]
+          end
+        end
+        page_count += 1
+      end
+      overall_links[:page_count] = page_count
+      return overall_links
+    end
+  end
+
+
+
+  desc "test", "test things"
+  method_option :environment,:default => 'production', :aliases => "-e", :desc => "Rails environment"
+  def test
+    load_rails(options[:environment])
+    puts "Page count #{Page.count}"
+  end
+  
+  desc "links", "Recreate links table (also recreates linkings)"
+  method_option :environment,:default => 'production', :aliases => "-e", :desc => "Rails environment"
+  method_option :verbose,:default => true, :aliases => "-v", :desc => "Output verbose progress"
+  def links
+    load_rails(options[:environment])
+    recreate_primary_links(options[:verbose])
+    recreate_linkings(options[:verbose])
+  end
+
+  desc "linkings", "Recreate linkings table"
+  method_option :environment,:default => 'production', :aliases => "-e", :desc => "Rails environment"
+  method_option :verbose,:default => true, :aliases => "-v", :desc => "Output verbose progress"
+  def linkings
+    load_rails(options[:environment])
+    recreate_linkings(options[:verbose])
+  end
+  
 end
 
-if !ENV["RAILS_ENV"] || ENV["RAILS_ENV"] == ""
-  ENV["RAILS_ENV"] = commandline_options[:environment]
-end
-
-require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
-
-# dump all content links and linkings
-Link.connection.execute('truncate table links;')
-Linking.connection.execute('truncate table linkings;')
-
-# primary content links - these are the links for each articles
-processed_count = 1
-puts "Creating content links for each article"
-Page.all.each do |page|
-  puts "Processing Page: #{page.id} ##{processed_count}"
-  page.create_primary_link
-  processed_count += 1
-end
-
-# processed_count = 1
-# puts "Processing in-article links"
-# Page.all.each do |article|
-#   puts "Processing Article: #{article.id} ##{processed_count}"
-#   links = article.convert_links
-#   article.save
-#   puts "Links: #{links.inspect}"
-#   processed_count += 1
-# end
+LinkManager.start
