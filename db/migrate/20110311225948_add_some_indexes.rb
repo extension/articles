5b3be9aa7fe9915d62370d42c0137de01124079b
Jason Adam Young <jasonadamyoung@gmail.com>
2011-03-13 13:31:06 -0400

    typo

--- db/migrate/20110311225948_add_some_indexes.rb
+++ db/migrate/20110311225948_add_some_indexes.rb
@@ -1,6 +1,6 @@
 class AddSomeIndexes < ActiveRecord::Migration
   def self.up
-    add_index("tagginges", ["taggable_id","taggable_type","tagging_kind"])
+    add_index("taggings", ["taggable_id","taggable_type","tagging_kind"])
   end
 
   def self.down
