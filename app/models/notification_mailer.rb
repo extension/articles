b8fc90f953e1ba2e4f29fbac5069a7638ec81bfe
Jason Adam Young <jayoung@extension.org>
2013-01-29 17:45:25 -0500

    leftover learn items

--- app/models/notification_mailer.rb
+++ app/models/notification_mailer.rb
@@ -355,20 +355,6 @@ class NotificationMailer < ActionMailer::Base
       @body           = {:isdemo => @isdemo, :token => token, :urls => urls }  
     end
    
-   # -----------------------------------
-   #  learn session
-   # -----------------------------------
-
-   def learn_upcoming_session(notification)
-     learn_session = LearnSession.find(notification.additionaldata[:learn_session_id])
-     # base parameters for the email
-     self.base_email(notification.notifytype_to_s)
-     @subject        = @subjectlabel+'Upcoming Learn Session'
-     @recipients     = notification.account.email
-     urls = Hash.new
-     urls['learnsession'] = url_for(:controller => 'learn', :action => :event, :id =>  learn_session.id)
-     @body = {:isdemo => @isdemo, :notification => notification, :learn_session => learn_session, :urls => urls}
-   end  
   
    # -----------------------------------
    #  system administration
