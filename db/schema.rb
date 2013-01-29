cb1c96748b207b0ee1948267c5a05a153bde6738
Jason Adam Young <jayoung@extension.org>
2013-01-29 17:47:54 -0500

    cleanup unused tags

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130129224225) do
+ActiveRecord::Schema.define(:version => 20130129224709) do
 
   create_table "accounts", :force => true do |t|
     t.string   "type",                                   :default => "",    :null => false
