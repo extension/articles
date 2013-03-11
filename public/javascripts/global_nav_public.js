373327ca4b4cd3364517b5fd0363b81ebc2da87a
benmac <ben.macneill@gmail.com>
2013-03-11 08:58:21 -0400

    global navbar js widget

---
+++ public/javascripts/global_nav_public.js
@@ -0,0 +1,31 @@
+var globalNav = '<style media="screen" type="text/css">\
+body {margin:0;}\
+.extension_global_nav {font: normal 13px/20px \'Helvetica Neue\', Helvetica, Arial, sans-serif;}\
+.extension_global_nav {text-align: center; margin:0;padding:.25em 0 .35em; background: #000;}\
+.extension_global_nav .group {display:inline;}\
+.extension_global_nav .group span {display:none;}\
+.extension_global_nav .group li {display:inline;}\
+.extension_global_nav ul {display:inline; padding:0; margin:0;}\
+.extension_global_nav li {display:inline; padding:0;}\
+.extension_global_nav .last {border:0;}\
+.extension_global_nav a {padding:.2em .5em; color:#999; text-decoration: none;}\
+.extension_global_nav a:hover {color: #fff;}\
+</style>\
+\
+\
+\
+<ul class="extension_global_nav public_nav">\
+  <li class="group"><span>Public sites</span>\
+    <ul>\
+      <li><a href="http://about.extension.org/" title="A starting place for all things eXtension.">About eXtension</a></li>\
+      <li><a href="http://ask.extension.org" title="One-to-one answers from a network of experts at universities across America.">Ask an Expert</a></li>\
+      <li><a href="http://campus.extension.org/ " title="Moodle courses developed by eXtension available to the public.">Campus</a></li>\
+      <li><a href="http://www.extension.org" title="A broad range of content from a network of experts at universities across America.">eXtension.org</a></li>\
+      <li><a href="http://learn.extension.org/" title="Professional development sessions on a variety of eXtension topics.">Learn</a></li>\
+      <li><a href="http://search.extension.org" title="One search for hundereds of Cooperative Extension sites.">Search</a></li>\
+    </ul>\
+  </li>\
+</ul>';
+(function() {
+    document.write(globalNav);
+})();
