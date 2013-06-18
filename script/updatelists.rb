94607a77b8e24e787e48f754d8b2754416f83f4d
Jason Adam Young <jayoung@extension.org>
2013-06-18 12:07:24 -0400

    List management

---
+++ script/updatelists.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/env ruby
+# === COPYRIGHT:
+#  Copyright (c) 2005-2009 North Carolina State University
+#  Developed with funding for the National eXtension Initiative.
+# === LICENSE:
+#  BSD(-compatible)
+#  see LICENSE file or view at http://about.extension.org/wiki/LICENSE
+
+require 'getoptlong'
+
+### Program Options
+progopts = GetoptLong.new(
+  [ "--environment","-e", GetoptLong::OPTIONAL_ARGUMENT ],
+  [ "--forceupdate","-f", GetoptLong::NO_ARGUMENT]
+)
+
+@environment = 'production'
+@forceupdate = false
+
+progopts.each do |option, arg|
+  case option
+    when '--environment'
+      @environment = arg
+    when '--forceupdate'
+      @forceupdate = true
+    else
+      puts "Unrecognized option #{opt}"
+      exit 0
+    end
+end
+### END Program Options
+
+if !ENV["RAILS_ENV"] || ENV["RAILS_ENV"] == ""
+  ENV["RAILS_ENV"] = @environment
+end
+
+require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
+
+#loop through all the managed lists and force the memberships to be updated
+MailmanList.managed.all.each do |list|
+  puts "Updating mailman information for: #{list.name}"
+  result = list.create_or_update_mailman_list(true)
+  puts "#{list.name} adds: #{result[:add_count]} removes: #{result[:remove_count]}"
+end
+
