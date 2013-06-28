15849bd89299c73441cd087a918b7537453f5a67
sdnall <s.daniel.nall@gmail.com>
2013-06-28 10:49:48 -0400

    use ssl

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,7 +1,7 @@
 Airbrake.configure do |config|
 		 config.api_key		 	= AppConfig.configtable['airbrake_api_key']
 		 config.host				= 'apperrors.extension.org'
-		 config.port				= 80
+		 config.port				= 443
 		 config.secure			= config.port == 443
 		 config.ignore       << "ActionController::UnknownHttpMethod"
 	 end
