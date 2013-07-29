9aabb31f6d51a28cbc47949d973d17aa7a583a82
Jason Adam Young <jayoung@extension.org>
2013-07-29 16:14:43 -0400

    hack the attachment_fu hack

--- vendor/plugins/attachment_fu/init.rb
+++ vendor/plugins/attachment_fu/init.rb
@@ -2,9 +2,13 @@ require 'tempfile'
 
 Tempfile.class_eval do
   # overwrite so tempfiles use the extension of the basename.  important for rmagick and image science
-  def make_tmpname(basename, n)
+  def make_tmpname(basename,n)
     ext = nil
-    sprintf("%s%d-%d%s", basename.to_s.gsub(/\.\w+$/) { |s| ext = s; '' }, $$, n, ext)
+    if(n)
+      sprintf("%s%d-%d%s", basename.to_s.gsub(/\.\w+$/) { |s| ext = s; '' }, $$, n, ext)
+    else
+      sprintf("%s%d-%s", basename.to_s.gsub(/\.\w+$/) { |s| ext = s; '' }, $$, ext)
+    end      
   end
 end
 
