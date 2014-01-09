cbfe4abf2f9cb5ed063dc190ead9a6de85c2ce58
Jason Adam Young <jayoung@extension.org>
2014-01-09 11:41:40 -0500

    Rails3 boot/environment changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,41 +1,75 @@
-# Settings specified here will take precedence over those in config/environment.rb
+Darmok::Application.configure do
+  # Settings specified here will take precedence over those in config/application.rb
 
-# production only gems
-config.gem 'airbrake'
+  # Code is not reloaded between requests
+  config.cache_classes = true
 
-# The production environment is meant for finished, "live" apps.
-# Code is not reloaded between requests
-config.cache_classes = true
+    # memcached
+  config.cache_store = :dalli_store
 
-# Full error reports are disabled and caching is turned on
-config.action_controller.consider_all_requests_local = false
-config.action_controller.perform_caching             = true
-config.action_view.cache_template_loading            = true
-config.active_record.colorize_logging = false
+  # Full error reports are disabled and caching is turned on
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
 
-# See everything in the log (default is :info)
-#config.log_level = :debug
+  # Disable Rails's static asset server (Apache or nginx will already do this)
+  config.serve_static_assets = false
 
-# Use a different logger for distributed setups
-# config.logger = SyslogLogger.new
+  # Compress JavaScripts and CSS
+  config.assets.compress = true
 
-# Use a different cache store in production
-# config.cache_store = :mem_cache_store
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = false
 
-# Enable serving of images, stylesheets, and javascripts from an asset server
-# config.action_controller.asset_host = "http://assets.example.com"
+  # Generate digests for assets URLs
+  config.assets.digest = true
 
-# Disable delivery errors, bad email addresses will be ignored
-# config.action_mailer.raise_delivery_errors = false
+  # Defaults to Rails.root.join("public/assets")
+  # config.assets.manifest = YOUR_PATH
 
-# Enable threaded mode
-# config.threadsafe!
+  # Specifies the header that your server uses for sending files
+  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
+  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
-# email settings
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.default_charset = "utf-8"
-config.action_mailer.smtp_settings = {
-  :address => "127.0.0.1",
-  :port => 25,
-  :domain => "extension.org"
-}
+  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  # config.force_ssl = true
+
+  # See everything in the log (default is :info)
+  # config.log_level = :debug
+
+  # Prepend all log lines with the following tags
+  # config.log_tags = [ :subdomain, :uuid ]
+
+  # Use a different logger for distributed setups
+  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+
+  # Use a different cache store in production
+  # config.cache_store = :mem_cache_store
+
+  # Enable serving of images, stylesheets, and JavaScripts from an asset server
+  # config.action_controller.asset_host = "http://assets.example.com"
+
+  # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
+  config.assets.precompile += %w( application-print.css )
+  config.assets.precompile += %w( public.css )
+  config.assets.precompile += %w( ie8.css )
+
+  # Disable delivery errors, bad email addresses will be ignored
+  # config.action_mailer.raise_delivery_errors = false
+
+  # Enable threaded mode
+  # config.threadsafe!
+
+  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
+  # the I18n.default_locale when a translation can not be found)
+  config.i18n.fallbacks = true
+
+  # Send deprecation notices to registered listeners
+  config.active_support.deprecation = :notify
+
+  # Log the query plan for queries taking more than this (works
+  # with SQLite, MySQL, and PostgreSQL)
+  # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  config.action_mailer.asset_host = "https://#{Settings.urlwriter_host}"
+  
+end
