d8b0f18d4e6f7320d1d33491ca5ba86a07256e9c
Jason Adam Young <jayoung@extension.org>
2014-01-29 13:20:18 -0500

    this shouldn't matter, but matching examples

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 # omniauth setup
-require 'omniauth-openid'
-require 'openid/store/filesystem'
 Rails.application.config.middleware.use OmniAuth::Builder do
+  require 'omniauth-openid'
+  require 'openid/store/filesystem'
   provider :open_id,  :store => OpenID::Store::Filesystem.new("#{Rails.root}/tmp/omniauth"), :name => 'people', :identifier => 'https://people.extension.org'
 end
 
