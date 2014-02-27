d36b912545ccdd7fb565b6a0ee7cac20ace088cc
Daniel Nall <s.daniel.nall@gmail.com>
2014-02-26 22:52:11 -0500

    new prod deploy location

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,3 +1,3 @@
-set :deploy_to, "/services/apache/vhosts/www.extension.org/railsroot/"
+set :deploy_to, "/services/www/"
 set :branch, 'master'
 server 'www.extension.org', :app, :web, :db, :primary => true
