0ea94b22730a0c0505e91357ee766aebff34e664
Jason Adam Young <jayoung@extension.org>
2014-02-27 14:06:14 -0500

    Fix the deprecation warning?

--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,9 @@ module Darmok
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # fix the Il8n deprecation
+    config.i18n.enforce_available_locales = true
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
