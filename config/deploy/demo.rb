a5905696201f07ab41a23907aa17a5932ce0d773
Daniel Nall <s.daniel.nall@gmail.com>
2014-03-04 09:51:45 -0500

    update deploy server options

--- config/deploy/demo.rb
+++
@@ -1,7 +0,0 @@
-set :deploy_to, "/services/apache/vhosts/www.demo.extension.org/railsroot/"
-if(branch = ENV['BRANCH'])
-  set :branch, branch
-else
-  set :branch, 'development'
-end
-server 'www.demo.extension.org', :app, :web, :db, :primary => true
--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -2,6 +2,6 @@ set :deploy_to, "/services/www/"
 if(branch = ENV['BRANCH'])
   set :branch, branch
 else
-  set :branch, 'ruby19rails3'
+  set :branch, 'master'
 end
 server 'dev-www.extension.org', :app, :web, :db, :primary => true
