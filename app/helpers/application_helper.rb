d1760f9424d0b16f5282b080ed7340952ea64fa6
Jason Adam Young <jayoung@extension.org>
2014-03-04 08:46:43 -0500

    attempt to rescue Nokogiri Syntax Errors
    https://github.com/sparklemotion/nokogiri/issues/881 doesn't look promising

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,23 +2,23 @@
 #  Copyright (c) 2005-2009 North Carolina State University
 #  Developed with funding for the National eXtension Initiative.
 # === LICENSE:
-# 
+#
 #  see LICENSE file
 
 module ApplicationHelper
-    
+
   def selected_tab_if(tab_name)
     'class="selected"' if @selected_tab == tab_name
   end
-  
+
   def is_submitter(question_response)
-    if question_response.submitter 
-      return true 
+    if question_response.submitter
+      return true
     else
       return false
     end
   end
-    
+
   #old time print
   def micro_time_print(time, class_name)
     return "" unless time
@@ -49,32 +49,32 @@ module ApplicationHelper
     end
     '<abbr class="' + class_name + '" title="' + time.strftime(mformat) + '">' + time.strftime(mdisplay) + '</abbr>'.html_safe
   end
-  
+
   def expanded_time_print(time)
     time.strftime("%B %d, %Y")
   end
-  
+
   def humane_date(time)
      if(time.blank?)
        ''
      else
        time.strftime("%B %e, %Y, %l:%M %p %Z")
      end
-  end 
-  
+  end
+
   # http://blog.macromates.com/2006/wrapping-text-with-regular-expressions/
   def wrap_text(txt, col=120)
     txt.gsub(/(.{1,#{col}})( +|$\n?)|(.{1,#{col}})/,
-      "\\1\\3\n") 
+      "\\1\\3\n")
   end
-  
+
   def article_summary(content)
     return nil unless content
     ptags = Regexp.new('<p>(.+?)<\/p>', Regexp::MULTILINE)
     find_sum = Regexp.new(Regexp.escape('<div id="summary" class="printme toc">') + "(.*?)" + Regexp.escape("</div>"), Regexp::MULTILINE)
-    
+
     summary_match = find_sum.match(content)
-    
+
   	if summary_match
   	  summary = summary_match[1]
   	else
@@ -82,39 +82,44 @@ module ApplicationHelper
       clean_content =  match ? strip_tags(match[1]) : ""
       summary = word_truncate(clean_content)
     end
-    
+
     return summary
   end
-  
+
   def format_text_for_display(content)
-    return word_wrap(simple_format(auto_link(content, :all, :target => "_blank"))) 
+    return word_wrap(simple_format(auto_link(content, :all, :target => "_blank")))
   end
-  
+
   private
-  
+
   def word_truncate(string, word_count=60, threshold=80)
     words = string.split(/\s+/)
-    
+
     if words.length < threshold
       result = string
     else
       result = words[0 .. word_count].join(" ") + " ..."
     end
-    
+
     return result
   end
-  
-  def get_title(html_content)
-    return "" unless html_content
-    parsed_html = Nokogiri::HTML::DocumentFragment.parse(html_content)
-    text = parsed_html.css("div#wow").text
-    if(text.blank?)
-      # fallback to the first paragraph
-      text = parsed_html.css("p").text
+
+  def get_wow_text(page)
+    html_content = page.content
+    return "" if html_content.blank?
+    begin
+      parsed_html = Nokogiri::HTML::DocumentFragment.parse(html_content)
+      text = parsed_html.css("div#wow").text
+      if(text.blank?)
+        # fallback to the first paragraph
+        text = parsed_html.css("p").text
+      end
+      return text
+    rescue Nokogiri::XML::XPath::SyntaxError
+      return page.title
     end
-    return text
   end
-  
+
   def select_category_tag(name, options ={}, selected_value =  nil)
     option_tags = ''
     Category.find_all.each{|category|
@@ -123,16 +128,16 @@ module ApplicationHelper
     option_tags += '</option>'
     select_tag(name, option_tags, options)
   end
-    
+
   def select_tag_for_community(options = {})
     tags = Tag.find(:all, :order => 'name').delete_if{|t| t.community}
 
     option_html = options_for_select(tags.map {|t| [t.name,t.id]})
-    
+
     select_tag('tag_id', option_html, options)
-    
+
   end
-  
+
   def select_tag_for_topic(community = nil)
     topics = Topic.find :all
     selected_arr = []
@@ -148,13 +153,13 @@ module ApplicationHelper
     option_html = options_for_select(topic_mapping, selected_arr)
     select_tag('community[public_topic_id]', option_html, {})
   end
-  
+
   def incomplete_profile(action)
     return '<h3>'+
           'Your profile must be complete to '+action+'.  '+link_to_unless_current("Complete profile.", edit_user_url(:id => current_user.id) )+
           '</h3>'
   end
-    
+
   def render_inline_logo(options = {})
     logo = options[:logo]
     return '' unless logo && logo.image?
@@ -164,11 +169,11 @@ module ApplicationHelper
     url += "?thumb=true" if show_thumbnail
     image_tag(url, :width => logo.width, :alt => alt_text)
   end
-    
+
   def link_by_count(count,text,params,htmloptions={})
     (count.to_i == 0) ? text : link_to(text,params,htmloptions)
   end
-  
+
   def default_location_id(fallback_location_id = nil)
     if(@personal[:location].blank?)
       if(fallback_location_id.blank?)
@@ -180,8 +185,8 @@ module ApplicationHelper
       return @personal[:location].id
     end
   end
-  
-  
+
+
   def default_county_id(fallback_county_id = nil)
     if(@personal[:county].blank?)
       if(fallback_county_id.blank?)
@@ -193,11 +198,11 @@ module ApplicationHelper
       return @personal[:county].id
     end
   end
-  
+
 
   def content_widget_styles
     css_data = File.new("#{Rails.root}/public/stylesheets/content_widget.css", 'r').read
     '<br /><style type="text/css" media="screen">' + css_data + '</style>'.html_safe
   end
-  
+
 end
