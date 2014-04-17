6b48846acd46bf1bd6e9c040a63b4f7c9465deba
Jason Adam Young <jayoung@extension.org>
2014-04-17 10:40:45 -0400

    get rid of ratom in feed generation

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -22,7 +22,7 @@ class FeedsController < ApplicationController
   end
         
   def content
-    begin
+    request.format = 'xml'
     filteredparameters_list = [:max_results,
                                {:limit => {:default => Settings.default_feed_content_limit}},
                                :tags,
@@ -36,11 +36,9 @@ class FeedsController < ApplicationController
       limit = filteredparams.limit
     end
     
-    # limit over max? let's be pedantic
     if(limit > Settings.max_feed_content_limit)
-      return render_feed_error({:errormessage => "Requested limit of #{limit} is greater than the max allowed: #{Settings.max_feed_content_limit}"})
+      limit = Settings.max_feed_content_limit
     end
-    
     
     # empty tags? - presume "all"
     if(filteredparams.tags.nil?)
@@ -71,79 +69,24 @@ class FeedsController < ApplicationController
     end
     
     if(alltags)
-       @returnitems = Page.recent_content(:datatypes => datatypes, :limit => limit)
+       @pages = Page.recent_content(:datatypes => datatypes, :limit => limit)
     else
-       @returnitems = Page.recent_content(:datatypes => datatypes, :content_tags => content_tags, :limit => limit, :tag_operator => tag_operator, :within_days => Settings.events_within_days)
+       @pages = Page.recent_content(:datatypes => datatypes, :content_tags => content_tags, :limit => limit, :tag_operator => tag_operator, :within_days => Settings.events_within_days)
     end
-              
-    title = "eXtension #{filteredparams.content_types.map{|name| name.capitalize}.join(',')}"
+           
+    @id = "tag:#{request.host},#{Time.now.year}:#{request.path}"  
+    @title = "eXtension #{filteredparams.content_types.map{|name| name.capitalize}.join(',')}"
     if(alltags)
-      title += "- All"
+      @title += "- All"
     else
-      title += "- " + content_tags.join(" #{filteredparams._tags.taglist_operator} ")
+      @title += "- " + content_tags.join(" #{filteredparams._tags.taglist_operator} ")
     end
 
-      
-    feed_meta = {:title => title, 
-                 :subtitle => "eXtension published content",
-                 :updated_at => @returnitems.blank? ? Time.zone.now : @returnitems.first.updated_at}
-    return render :text => atom_feed_from(@returnitems, feed_meta), :content_type => Mime::ATOM
-  rescue
-    do_404
-  end
-  
-  end
-      
-  private
-  
-  def render_feed_error(feedoptions={})
-    feed = Atom::Feed.new do |f|
-      f.title = "eXtension Feed Error"
-      # TODO : link over to activity?
-      f.links << Atom::Link.new(:rel => 'alternate', :type => 'text/html', :href => feedoptions[:alternate] || (request.protocol + request.host_with_port))
-      f.links << Atom::Link.new(:rel => 'self', :type => 'application/atom+xml', :href => feedoptions[:self] || request.url)
-      f.updated = Time.now.utc.xmlschema
-      f.id = make_atom_feed_id()
-      f.entries << Atom::Entry.new do |e|
-        e.authors << Atom::Person.new(:name => 'eXtension', :email => 'webmaster@extension.org')
-        e.title = "eXtension Feed Error"
-        e.links << Atom::Link.new(:rel => 'alternate', :type => 'text/html', :href => feedoptions[:alternate] || (request.protocol + request.host_with_port))
-        e.id = make_atom_entry_id("Invalid")
-        e.updated = Time.now.utc.xmlschema
-        errormsg = feedoptions[:errormessage] || "An error occurred with the feed you requested."
-        e.content = Atom::Content::Html.new("<p>#{errormsg}</p>")
-      end
-    end
-
-    render :xml => feed.to_xml    
+    @subtitle = "eXtension published content"
+    @updated_at = @pages.blank? ? Time.zone.now : @pages.first.updated_at
+    respond_to do |format|
+      format.xml { render(:layout => false, template: 'feeds/pages', :content_type => "application/atom+xml") }
+    end  
   end
 
-  
-  def atom_feed_from(entries, meta)
-    feed = Atom::Feed.new do |f|
-      f.title = meta[:title]
-      f.subtitle = meta[:subtitle]
-      f.links << Atom::Link.new(:type => "application/atom+xml", :rel => "self", :href => meta[:self] || request.url)
-      f.links << Atom::Link.new(:type => "text/html", :rel => "alternate", :href => meta[:alternate] || url_for(:only_path => false, :controller => 'main', :action => 'index'))
-      f.updated = meta[:updated_at]
-      f.authors << Atom::Person.new(:name => 'Contributors')
-      f.id = make_atom_feed_id()
-      for entry in entries
-        f.entries << entry.to_atom_entry
-      end
-    end
-    feed.to_xml
-  end
-  
-  def make_atom_feed_id(schema_date=Time.now.year)
-    "tag:#{request.host},#{schema_date}:#{request.path}"
-  end
-  
-  def make_atom_entry_id(obj,schema_date=Time.now.year)
-    if(obj.class != "String")
-      "tag:#{request.host},#{schema_date}:#{obj.class}/#{obj.id}"
-    else
-      "tag:#{request.host},#{schema_date}:#{obj}"
-    end
-  end
 end
