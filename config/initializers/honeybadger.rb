728b4af2e1415b7ed374ba74b0656379e1a20c0b
Jason Adam Young <jayoung@extension.org>
2014-10-19 14:39:27 -0400

    Ignore UnknownHTTPMethod

--- config/initializers/honeybadger.rb
+++ config/initializers/honeybadger.rb
@@ -1,4 +1,5 @@
 Honeybadger.configure do |config|
   config.api_key = Settings.honeybadger_api_key
   config.environment_name = Settings.app_location
+  config.ignore       << "ActionController::UnknownHttpMethod"
 end
