8c30165238b80ab5aaafdce00a39a774c1218720
Jason Adam Young <jayoung@extension.org>
2014-12-02 17:33:43 -0500

    news cleanup script (still WIP)

---
+++ script/one-time/news-cleanup.rb
@@ -0,0 +1,94 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+
+# load rails
+if !ENV["RAILS_ENV"] || ENV["RAILS_ENV"] == ""
+  ENV["RAILS_ENV"] = 'development'
+end
+require_relative("../../config/environment")
+
+class CreateNodeWorkflow < ActiveRecord::Base
+  # connects to the create database
+  self.establish_connection :create
+  self.set_table_name 'node_workflow'
+  self.set_primary_key "nwid"
+end
+
+
+class CreateNodeWorkflowEvent < ActiveRecord::Base
+  # connects to the create database
+  self.establish_connection :create
+  self.set_table_name 'node_workflow_events'
+  self.set_primary_key "weid"
+end
+
+class CreateNode < ActiveRecord::Base
+  # connects to the create database
+  self.establish_connection :create
+  self.set_table_name 'node'
+  self.set_primary_key "nid"
+  self.inheritance_column = "none"
+  bad_attribute_names :changed
+end
+
+# let's delete some buckets
+ContentBucket.where("name IN ('news','originalnews','notnews')").each do |bucket|
+  bucket.destroy
+end
+
+
+# let us loop through the news, update the workflow,
+# create some workflow events, and then destroy the page
+
+# limited to two at a time for testing right now
+Page.where(datatype: 'News').limit(2).each do |page|
+# Page.where(datatype: 'News').find_each do |page|
+  puts "Processing Page ##{page.id}:"
+  node_id = page.create_node_id
+
+  if(create_node = CreateNode.where(nid: node_id).first)
+    puts "... found node ##{create_node.nid} (#{create_node.type})."
+    if(cnw = CreateNodeWorkflow.where(node_id: node_id).last)
+      unix_timestamp = Time.now.utc.to_i
+      cnw.update_attributes(active: false,
+                            status_text: 'Draft',
+                            status: 1,
+                            review_count: 0,
+                            draft_status: nil,
+                            draft_status_text: nil,
+                            published_at: nil,
+                            unpublished_at: unix_timestamp,
+                            published_revision_id: nil,
+                            changed: unix_timestamp)
+      puts "... updated node workflow (id #{cnw.nwid})."
+
+      # need some workflow events
+      CreateNodeWorkflowEvent.create(node_id: node_id,
+                                     node_workflow_id: cnw.nwid,
+                                     user_id: 1,
+                                     revision_id: cnw.current_revision_id,
+                                     event_id: 6,
+                                     description: 'unpublished',
+                                     created: unix_timestamp)
+
+      CreateNodeWorkflowEvent.create(node_id: node_id,
+                                     node_workflow_id: cnw.nwid,
+                                     user_id: 1,
+                                     revision_id: cnw.current_revision_id,
+                                     event_id: 7,
+                                     description: 'made inactive',
+                                     created: unix_timestamp)
+
+      puts "... logged workflow events."
+
+    else
+      puts "... unable to find a workflow entry for this node (create node id: #{node_id})."
+    end
+  else
+    puts "... did not find a Create Node for this page."
+  end
+
+  page.destroy
+  puts "... destroyed page."
+
+end
