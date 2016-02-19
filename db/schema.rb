c6b488d4ba683b86f7b6fcd473cdca4f2a49ce73
Jason Adam Young <jayoung@extension.org>
2016-02-19 11:03:18 -0500

    pull db attribute (full cleanup mode)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150928160946) do
+ActiveRecord::Schema.define(:version => 20160219160222) do
 
   create_table "admin_logs", :force => true do |t|
     t.integer  "person_id",                :default => 0, :null => false
@@ -336,7 +336,6 @@ ActiveRecord::Schema.define(:version => 20150928160946) do
     t.string   "pinterest_handle"
     t.string   "gplus_handle"
     t.text     "twitter_widget"
-    t.boolean  "show_donation",           :default => true
     t.integer  "primary_tag_id"
   end
 
