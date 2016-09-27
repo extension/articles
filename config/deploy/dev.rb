510e66aa63b744dd2c54a06a8a808abafba40d52
Jason Adam Young <jayoung@extension.org>
2016-09-27 17:28:57 -0400

    temp deployment changes

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -4,5 +4,6 @@ if(branch = ENV['BRANCH'])
 else
   set :branch, 'master'
 end
-set :vhost, 'dev-articles.extension.org'
+set :vhost, 'dev-articles.awsi.extension.org'
 server vhost, :app, :web, :db, :primary => true
+set :port, 22
--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -2,3 +2,4 @@ set :deploy_to, "/services/articles/"
 set :branch, 'master'
 set :vhost, 'articles.extension.org'
 server vhost, :app, :web, :db, :primary => true
+set :port, 24
