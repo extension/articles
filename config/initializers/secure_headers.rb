41112c6e7890ecd3515e8615c1577e042b9dc225
Jason Adam Young <jayoung@extension.org>
2017-01-23 11:49:35 -0500

    domain typo

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -4,7 +4,7 @@ SecureHeaders::Configuration.default do |config|
     default_src: %w('self'),
     img_src: %w(* data:),
     script_src: %w('self' 'unsafe-inline' *.extension.org www.google-analytics.com platform.twitter.com),
-    font_src: %w('self' fonts.gtstatic.com),
+    font_src: %w('self' fonts.gstatic.com),
     style_src: %w('self' fonts.googleapis.com 'unsafe-inline'),
     object_src: %w('self' create.extension.org)
   }
