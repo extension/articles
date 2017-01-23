13336a067626ec2a4b7d7267faf102b0f13f29d0
Jason Adam Young <jayoung@extension.org>
2017-01-23 11:38:10 -0500

    more tweaks

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -1,8 +1,10 @@
 SecureHeaders::Configuration.default do |config|
   config.csp = {
-  # directive values: these values will directly translate into source directives
-  default_src: %w('self'),
-  img_src: %w(* data:),
-  script_src: %w('self')
-}
+    # directive values: these values will directly translate into source directives
+    default_src: %w('self'),
+    img_src: %w(* data:),
+    script_src: %w('self', 'unsafe-inline', *.extension.org),
+    style_src: %w('self' fonts.googleapis.com),
+    object_src: %w('self', create.extension.org)
+  }
 end
