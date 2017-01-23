94c42e237751da870717d61c82bda1f8410de2bb
Jason Adam Young <jayoung@extension.org>
2017-01-23 11:48:01 -0500

    more tweaks

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -3,8 +3,9 @@ SecureHeaders::Configuration.default do |config|
     # directive values: these values will directly translate into source directives
     default_src: %w('self'),
     img_src: %w(* data:),
-    script_src: %w('self' 'unsafe-inline' *.extension.org),
-    style_src: %w('self' fonts.googleapis.com),
+    script_src: %w('self' 'unsafe-inline' *.extension.org www.google-analytics.com platform.twitter.com),
+    font_src: %w('self' fonts.gtstatic.com),
+    style_src: %w('self' fonts.googleapis.com 'unsafe-inline'),
     object_src: %w('self' create.extension.org)
   }
 end
