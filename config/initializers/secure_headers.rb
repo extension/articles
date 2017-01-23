4729c428c370f25c349d1d4d49dd0984c35c85ea
Jason Adam Young <jayoung@extension.org>
2017-01-23 11:19:24 -0500

    change some settings, leaving most at defaults I hope

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -1,2 +1,8 @@
 SecureHeaders::Configuration.default do |config|
+  config.csp = {
+  # directive values: these values will directly translate into source directives
+  default_src: %w('self'),
+  img_src: %w(* data:),
+  script_src: %w('self')
+}
 end
