c77663245df7bdf851718e0690616068568240ff
Jason Adam Young <jayoung@extension.org>
2017-01-23 11:58:04 -0500

    more

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -3,9 +3,10 @@ SecureHeaders::Configuration.default do |config|
     # directive values: these values will directly translate into source directives
     default_src: %w('self'),
     img_src: %w(* data:),
-    script_src: %w(* 'self' 'unsafe-inline'),
-    font_src: %w(* 'self'),
-    style_src: %w(* 'self' 'unsafe-inline'),
-    object_src: %w('self' create.extension.org)
+    script_src: %w(* 'unsafe-inline'),
+    font_src: %w(*),
+    style_src: %w(* 'unsafe-inline'),
+    object_src: %w('self' create.extension.org),
+    frame_src: %w(*)
   }
 end
