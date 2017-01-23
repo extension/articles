13a57ace33153e8718b4a6c28deb741134521332
Jason Adam Young <jayoung@extension.org>
2017-01-23 11:41:55 -0500

    syntax errors

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -3,8 +3,8 @@ SecureHeaders::Configuration.default do |config|
     # directive values: these values will directly translate into source directives
     default_src: %w('self'),
     img_src: %w(* data:),
-    script_src: %w('self', 'unsafe-inline', *.extension.org),
+    script_src: %w('self' 'unsafe-inline' *.extension.org),
     style_src: %w('self' fonts.googleapis.com),
-    object_src: %w('self', create.extension.org)
+    object_src: %w('self' create.extension.org)
   }
 end
