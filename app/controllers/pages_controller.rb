16260c1e46812ad98558b682673ca7bb86d2c1ff
Jason Young <jay@outfielding.net>
2018-06-14 11:13:13 -0400

    change to wordpress "Post" type

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -259,10 +259,10 @@ class PagesController < ApplicationController
 
   def wxr
     @page = Page.find(params[:id])
-    @post_type = 'page'
     # reprocess the links in the content in order to make absolute links
     @page.convert_links(true)
     @absolute_linked_content = @page.content
+    @post_type = 'post'
     request.format = 'xml'
     respond_to do |format|
       format.xml { render(:layout => false, :content_type => "application/rss+xml") }
